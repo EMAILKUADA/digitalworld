72a5d7654e845454e88d75b7bfef50b712587bbe
EMAILKUADA <emailkuada7@gmail.com>
2026-09-11 10:31:20 +0800

    Refactor next.config.ts for clarity and updates

--- next.config.ts
+++ next.config.ts
@@ -45,18 +45,15 @@ const nextConfig: NextConfig = {
           { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
         ],
       },
-      {
+            {
         source: '/(.*)',
         headers: [
-          { key: 'Content-Security-Policy', value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' https: wss: data: blob:;" },
+          { key: 'Content-Security-Policy', value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' https: wss: data: blob:; frame-ancestors 'self' https://anamidigital.weebly.com https://vercel.app;" },
           { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
           { key: 'X-XSS-Protection', value: '1; mode=block' },
         ],
       },
-    ];
-  },
-};
+
 
 export default nextConfig;
